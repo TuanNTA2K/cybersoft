057cb663e166d0a4c91b93e4d80af41209997123
TuanNTA2kIT <nguyenthanhanhtuan123@gmail.com>
2023-12-16 23:23:33 +0700

    nop bai tap 1 springboot

---
+++ spring boot/baitapspringboot04/baitap1springboot04/src/main/java/com/cybersoft/baitap1springboot04/Baitap1springboot04Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.baitap1springboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap1springboot04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baitap1springboot04Application.class, args);
+	}
+
+}
